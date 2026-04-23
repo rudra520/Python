915679fd04d77c7afebbefc5faf2e3e04bb8df97
royalking <rudra222000@gmail.com>
2026-04-23 22:43:03 +0530

    add

---
+++ cisco/Function/Function-1.py
@@ -0,0 +1,8 @@
+def num():
+    input_num = int(input("Enter a number: "))
+    if input_num % 2 == 0:
+        print("The number is even.")    
+    else:
+        print("The number is odd.")
+print("This program determines if a number is even or odd.")
+num()
