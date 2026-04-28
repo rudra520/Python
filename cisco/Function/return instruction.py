e9807239fa71c0e01f0bc949970e05b8dad09a04
royalking <rudra222000@gmail.com>
2026-04-28 12:52:02 +0530

    Learning use of return in function.

---
+++ cisco/Function/return instruction.py
@@ -0,0 +1,34 @@
+'''Effects and results: the return instruction'''
+#4.3.1return without an expression
+#return without an expression.
+#Without any argument 
+def happy_new_year(wishes = True):
+    print("Three...")
+    print("Two...")
+    print("One...")
+    if not wishes:
+        return
+
+    print("Happy New Year!")
+
+happy_new_year()
+
+# with Fasle as an argument 
+happy_new_year(False)
+
+#return with an expression
+
+def boring_function():
+    return 123
+
+x = boring_function()
+
+print("The boring_function has returned its result. It's:", x)
+# Fun
+def boring_function():
+    print("'Boredom Mode' ON.")
+    return 123
+
+print("This lesson is interesting!")
+boring_function()
+print("This lesson is boring...")
