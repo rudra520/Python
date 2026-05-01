79cf0b47c03537b8d0242bd290175b6af1c280f2
royalking <rudra222000@gmail.com>
2026-05-01 17:59:32 +0530

    Python essential 1 , Function

---
+++ cisco/Function/main (7).py
@@ -0,0 +1,5 @@
+#4.3.4   LAB   A leap year: writing your own functions
+#Your task is to write and test a function which takes one argument (a year) and returns True if the year is a leap year, or False otherwise.
+#The seed of the function is already shown in the skeleton code in the editor.
+#Note: we've also prepared a short testing code, which you can use to test your function.
+#The code uses two lists ‒ one with the test data, and the other containing the expected results. The code will tell you if any of your results are invalid.
