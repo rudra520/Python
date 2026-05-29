d112c2cada2f19a395b19a6b1bdb584be2355614
Royalking0016 <rudra222000@gmail.com>
2026-05-30 02:06:32 +0530

    3.2.5 Looping your code with for
    3.2.5 Looping your code with for

---
+++ cisco/Loops/3.2.5 Looping your code with for.py
@@ -0,0 +1,13 @@
+# syantax while loop
+i = 0
+while i < 100:
+    # do_something()
+    i += 1
+# Syantax for loop
+# for i in range(100):
+#     # do_something()
+#     pass
+
+for i in range(2, 8):
+    print("The value of i is currently", i)
+
